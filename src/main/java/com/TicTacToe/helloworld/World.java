2d225545960939a0ae0af22a1a43611fafefc60e
Harpa Guðjónsdóttir <harpa09@ru.is>
2016-10-24 20:31:50 +0000

    Added gradle and build scripts

---
+++ src/main/java/com/TicTacToe/helloworld/World.java
@@ -0,0 +1,12 @@
+package com.TicTacToe.helloworld;
+import net.joningi.icndb.ICNDBClient;
+import net.joningi.icndb.Joke;
+
+public class World {
+
+    private String greeting = "Hello world!";
+
+    public String greet() {
+        return greeting;
+    }
+}
